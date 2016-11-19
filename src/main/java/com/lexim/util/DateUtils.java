d87a607b944e27f1c465bbb6f374497fd9145d28
Junald Bayog <junald.bayog@orangeandbronze.com>
2016-11-19 22:54:51 +0800

    Add date utils for converting LocalDate object to Date object.

---
+++ src/main/java/com/lexim/util/DateUtils.java
@@ -0,0 +1,12 @@
+package com.lexim.util;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateUtils {
+	
+	public static Date asDate(LocalDate localDate) {
+		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
+}
